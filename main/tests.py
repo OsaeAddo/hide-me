5db4b07ef484d27cbd8ce6bff0769a837c891c52
OsaeAddo <immanueloaddo@gmail.com>
2021-10-12 14:36:36 +0000

    write home page tests for checking status code

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test.testcases import SimpleTestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from .views import HomePageView
+
+class HomePageTests(SimpleTestCase):
+
+    def setup(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    
