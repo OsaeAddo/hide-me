2c933db538cab972ef2975c007b933c0958ee006
OsaeAddo <immanueloaddo@gmail.com>
2021-10-12 14:23:38 +0000

    define Message model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from users.models import CustomUser
+class Message(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    message = models.TextField(max_length=330, null=False, blank=False)
+    message_sent_date = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        verbose_name_plural = 'Messages'
+
+    def __str__(self):
+        return f'{self.user.username} just received an anonymous message'
