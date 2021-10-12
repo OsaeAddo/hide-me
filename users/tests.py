96cddef02e4f2f26b807cac98301ce6730509b3c
OsaeAddo <immanueloaddo@gmail.com>
2021-10-12 13:47:53 +0000

    write tests for if user account creation fails

--- users/tests.py
+++ users/tests.py
@@ -15,3 +15,19 @@ class CustomUserTests(TestCase):
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_superuser)
         self.assertFalse(user.is_staff)
+
+    def test_user_not_created(self):
+        User = get_user_model()
+        user = User.objects.create(
+            username = 'Dede',
+            email = 'dede@gmail.com',
+            password = 'testpass321',
+        )
+        self.assertFalse(user.username, 'Dede')
+        self.assertFalse(user.email, 'dede@gmail.com')
+        self.assertFalse(user.is_active)
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+
+
+    
